3d3c754054c6970f27e26d86a499f7cdeeb9b67d
Janis Zisenis <janis.zisenis@hotmail.de>
2018-04-02 10:36:54 +0200

    project setup

---
+++ src/test/java/BowingGameTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BowingGameTest {
+
+    @Test
+    void nothing() {
+        assertFalse(false, "This is a null test!");
+    }
+
+}
