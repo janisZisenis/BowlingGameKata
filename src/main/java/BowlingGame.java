ddd0e41ed5d89868200f5ab91fe2379b22f1bf6a
Janis Zisenis <janis.zisenis@hotmail.de>
2018-04-02 11:52:33 +0200

    refactors

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,14 +1,10 @@
 public class BowlingGame {
 
-    int rolledPins = 0;
-
     int[] rolls = new int[21];
     int currentRoll = 0;
 
     public void roll(int pins) {
-        rolledPins += pins;
-        rolls[currentRoll] = pins;
-        currentRoll++;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
@@ -16,17 +12,35 @@ public class BowlingGame {
 
         int frameIndex = 0;
         for(int frame = 0; frame < 10; frame++) {
-            if(isSpare(frameIndex)) {
-                score += 10 + rolls[frameIndex + 2];
+            if(isStrike(frameIndex)) {
+                score += 10 + strikeBonus(frameIndex++);
+            } else if(isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            } else {
+                score += pinsInFrame(frameIndex);
+                frameIndex += 2;
             }
-            else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
-            }
-            frameIndex += 2;
+
         }
 
-
         return score;
+    }
+
+    private int pinsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
     }
 
     private boolean isSpare(int frameIndex) {
